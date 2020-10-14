f0d2c16ca97e28238396d596fd8c5fb63d8856e1
Jacob Coneby <jkconeby@gmail.com>
2020-10-13 20:32:03 -0400

    Update CallOnTheHunt.ps1

--- CallOnTheHunt.ps1
+++ CallOnTheHunt.ps1
@@ -1,140 +1,112 @@
-﻿function Get-HostInfo {
-
-$hostInfo = [PSCustomObject] @{
-        Process           = ''
-        Service           = ''
-        OS                = Get-WmiObject Win32_OperatingSystem
-        LocalGroups       = ''
-        LocalGroupMembers = ''
-        Users             = ''
-        LoggedOnUsers     = ''
-        Logins            = wmic netlogin get fullname,userid,lastlogon,logonhours,privileges
-        SchTasks          = ''
-        Connections       = ''
-        Shares            = Get-WmiObject -ClassName Win32_Share
-        RegistryStartups  = ''
-        RegistrySchTasks  = Get-ChildItem 'HKLM:\HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree' -Recurse | Select Name
-        CriticalEvents    = ''
-        GroupedEvents     = ''
-        Baseline          = ''
-        RecentModFiles    = ''
-        Firewall          = ''
-        Prefetch          = ''
-        Report            = ''
-        }
-        
-return $hostInfo
-
-}
-
-
-function Format-Report {
-Param([Parameter(Mandatory=$True
-,HelpMessage="Error: Please enter an object")]$Object)
-
- Write-Output "----------------------------Process List---------------------------------"
- Write-Output $Object.Process | FT
- Write-Output " "
- Write-Output "---------------------------Service List----------------------------------------------------------------"
- Write-Output $Object.Service | Select Name,State,ProcessName,ProcessId,Pathname | Sort-Object -Property State,ProcessName | FT
- Write-Output " "
- Write-Output "---------------------------Connections--------------------------------------------"
- Write-Output $Object.Connections | FT
- Write-Output " "
- Write-Output "---------------------------OS--------------------------------------------"
- Write-Output $Object.OS | Select Caption,Version,RegisteredUser,SystemDirectory,SerialNumber | FT
- Write-Output " "
- Write-Output "---------------------------LocalGroups--------------------------------------------"
- Write-Output $Object.LocalGroups | FT
- Write-Output " "
- Write-Output "---------------------------LocalGroupMembers--------------------------------------------"
- Write-Output $Object.LocalGroupMembers | Select GroupName,Member,PSComputerName
- Write-Output " "
- Write-Output "---------------------------Users--------------------------------------------"
- Write-Output $Object.Users | FT
- Write-Output " "
-  Write-Output " "
- Write-Output "---------------------------Logged On Users--------------------------------------------"
- Write-Output $Object.LoggedOnUsers | FT
- Write-Output " "
- Write-Output "---------------------------Logins--------------------------------------------"
- Write-Output $Object.Logins | FT
- Write-Output " "
- Write-Output "---------------------------Scheduled Tasks--------------------------------------------"
- Write-Output $Object.SchTasks | FT
- Write-Output " "
- Write-Output "---------------------------Registry Scheduled Tasks--------------------------------------------"
- Write-Output $Object.RegistrySchTasks | FT
- Write-Output " "
- Write-Output "---------------------------Shares--------------------------------------------"
- Write-Output $Object.Shares | FT
- Write-Output " "
- Write-Output "---------------------------Grouped Events--------------------------------------------"
- Write-Output $Object.GroupedEvents | FT
- Write-Output " "
- Write-Output "---------------------------Registry Start Ups--------------------------------------------"
- Write-Output $Object.RegistryStartups | FT
- Write-Output " "
- Write-Output "---------------------------Firewall--------------------------------------------"
- Write-Output $Object.Firewall | Select InstanceID,LocalAddress,LocalPort,RemoteAddress,RemotePort,Direction,Action | FT
- Write-Output " "
- Write-Output "---------------------------Prefetch--------------------------------------------"
- Write-Output $Object.Prefetch | FT
-
-}
-
 #Change creds as needed
 $Username = 'Admin'
-$Password = 'DomainAdminPassword'
+$Password = 'Password'
 
 #Create Credential Object
 [SecureString]$secureString = $Password | ConvertTo-SecureString -AsPlainText -Force
 [PSCredential]$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $secureString
 
-
-#import the hunt.psm1 module saved on your desktop
-$Path = ($env:HOMEDRIVE + $env:HOMEPATH + '\Desktop\')
-Import-Module ($Path + $target + "\Hunt.psm1")
+<#Import the hunt.psm1 module saved on your desktop
+May need to change $Path variable #>
 
 
-#Will look at desktop for registry list and Windows Event Log
-$regArray = Get-Content -Path ($Path + '\registry_keys.txt')
-$eventCSV = Import-CSV -Path ($Path + '\Windows_Event_Log.csv')
-
-
-#Can change this to take content from a text or csv file
-$targets = @('192.168.253.145','localhost')
-
-foreach ($target in $targets)
-{
-    
-$machine = Invoke-Command -ComputerName $target -Credential $creds -ScriptBlock ${Function:Get-HostInfo}
-
-#Add additional properties
-$machine.Process           = Get-WmiProcess -ComputerName $target -Credential $creds
-$machine.Service           = Get-WmiService -ComputerName $target -Credential $creds
-$machine.LocalGroups       = Get-LGroup -ComputerName $target -Credential $creds
-$machine.Users             = Get-LUser -ComputerName $target -Credential $creds
-$machine.LocalGroupMembers = Get-LGroupMembers -ComputerName $target -Credential $creds
-$machine.LoggedOnUsers     = Get-LoggedOnUser -ComputerName $target -Credential $creds
-$machine.SchTasks          = Get-SchTask -ComputerName $target -Credential $creds
-$machine.Connections       = Get-Connection -ComputerName $target -Credential $creds
-#Edit the StartPath parameter as needed
-$machine.Baseline          = Get-BaselineHash -ComputerName $target -Credential $creds -StartPath "C:\Users\jkcon\OneDrive\Documents"
-#Edit the StartPath and Days parameter as needed
-$machine.RecentModFiles    = Get-RecentModFile -ComputerName $target -Credential $creds -StartPath "C:\Users\jkcon\OneDrive\Documents" -Days 2
-$machine.Firewall          = Survey-Firewall -ComputerName $target -Credential $creds
-$machine.LoggedOnUsers     = Get-LoggedOnUser -ComputerName $target -Credential $creds  
-$machine.RegistryStartups  = Get-Registry -regKeyArray $regArray -ComputerName $target -Credential $creds
-$machine.Prefetch          = Get-Prefetch -ComputerName $target -Credential $creds
-$machine.CriticalEvents    = Get-ImportantEvent -EventList $eventCSV -BeginTime (Get-Date '08/25/2020') -EndTime (Get-Date '08/31/2020') -ComputerName $target -Credential $creds
-$machine.GroupedEvents     = Group-Event -eventRecord $machine.CriticalEvents -eventList $eventCSV
-
-
-$machine.Report            = Format-Report -Object $machine
-
-$machine | Export-Clixml -Path ($Path + $target + ".xml")
-echo $machine.Report > ($Path + $Target + ".txt")
-
+$desktopPath = [string]::Concat($env:HOMEDRIVE,$env:HOMEPATH,'\Desktop\')
+$outputFolder = [string]::Concat($env:HOMEDRIVE,$env:HOMEPATH,'\Desktop\Output\')
+if(!(Test-Path $outputFolder)) {
+    New-Item -Path $outputFolder -ItemType Directory
 }
+
+Import-Module ($desktopPath + "Hunt.psm1")
+
+$eventCSV = Import-CSV -Path ($desktopPath + "Windows_Event_Log.csv")
+$regArray = Get-Content -Path ($desktopPath + "registry_keys.txt")
+$regIOCcsv   = Import-CSV -Path ($desktopPath + "registry_IOC.csv")
+
+#Adjust these dates as needed
+$After = Get-Date ('08/30/2020')
+$Before = Get-Date ('09/01/2020')
+
+#Change targets as needed
+$targets = Import-Csv -Path ($desktopPath + "Targets.csv")
+$targets = $targets.Hosts
+<#Least Frequncy Analysis - Change the threshold
+May want to make the threshold a percentage of the $targets.
+Example $threshold = (0.10 * $targets.count #>
+$threshold = 2
+
+
+$Process           = Get-WmiProcess -ComputerName $targets -Credential $creds
+$ProcessLFA        = Group-ProcessByPath -processObject $Process -Threshold $threshold
+$Service           = Get-WmiService -ComputerName $targets -Credential $creds
+$OS                = Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {Get-WmiObject Win32_OperatingSystem}
+$LocalGroups       = Get-LGroup -ComputerName $targets -Credential $creds
+$LocalGroupMembers = Get-LGroupMembers -ComputerName $targets -Credential $creds
+$Users             = Get-LUser -ComputerName $targets -Credential $creds
+$LoggedOnUsers     = Get-LoggedOnUser -ComputerName $targets -Credential $creds
+$SchTasks          = Get-SchTask -ComputerName $targets -Credential $creds
+$Connections       = Get-Connection -ComputerName $targets -Credential $creds
+$Shares            = Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {Get-WmiObject -ClassName Win32_Share}
+$RegistryStartups  = Get-Registry -regKeyArray $regArray -ComputerName $targets -Credential $creds
+$RegistrySchTasks  = Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {Get-ChildItem 'HKLM:\HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree' -Recurse | Select Name,PSComputerName}
+$RegistryIOC       = Get-RegistryIOC -RegList $regIOCcsv -ComputerName $targets -Credential $creds
+$SecurityLog       = Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {Get-EventLog -LogName Security -After $Using:After -Before $Using:Before}
+$SecurityLogFormat = Format-Events -logs $SecurityLog
+$CriticalEvents    = $SecurityLogFormat | Where {$_.AlertMessage -ne $null} | Select Time,IP,Index,EventID,AccountName,AlertMessage | Sort-Object -Property Time | FT
+$GroupedEvents     = Group-SecurityEventID -eventRecord $SecurityLogFormat -eventList $eventCSV
+
+#Change the startpath and files modified in the last amount of days as needed
+$BaselineHash      = Get-BaselineHash -ComputerName $targets -Credential $creds -StartPath "C:\"
+$RecentModFiles    = Get-RecentModFile -ComputerName $targets -Credential $creds -StartPath "C:\" -Days 30
+$Firewall          = Survey-Firewall -ComputerName $targets -Credential $creds
+$Prefetch          = Get-Prefetch -ComputerName $targets -Credential $creds
+
+
+#Export the variables to CSV files
+$Process | Export-CSV -Path ($outputFolder + "process.csv") -NoTypeInformation
+$ProcessLFA | Select Count,FilePath,Name,PSComputerName,Hash | Export-CSV -Path ($outputFolder + "processLFA.csv") -NoTypeInformation
+$Service | Export-CSV -Path ($outputFolder + "service.csv") -NoTypeInformation
+$OS | Select PSComputerName,CSName,InstallDate,LastBootUpTime,Name,Version | Export-CSV -Path ($outputFolder + "OS.csv") -NoTypeInformation
+$LocalGroups | Select PSComputerName,Caption,Name,SID,Description | Export-CSV -Path ($outputFolder + "LocalGroups.csv") -NoTypeInformation
+$LocalGroupMembers | Export-CSV -Path ($outputFolder + "LocalGroupMembers.csv") -NoTypeInformation
+$Users | Select PSComputerName,Caption,Name,SID | Export-CSV -Path ($outputFolder + "users.csv") -NoTypeInformation
+$LoggedOnUsers | Export-CSV -Path ($outputFolder + "LoggedOnUsers.csv") -NoTypeInformation
+$SchTasks | Select PSComputerName,TaskName,Author,Date,TaskPath,URI,Description | Export-CSV -Path ($outputFolder + "schtasks.csv") -NoTypeInformation
+$Connections | Export-CSV -Path ($outputFolder + "connections.csv") -NoTypeInformation
+$Shares | Select PSComputerName,Name,Path,Description | Export-CSV -Path ($outputFolder + "shares.csv") -NoTypeInformation
+$RegistryStartups | Export-CSV -Path ($outputFolder + "RegistryStartups.csv") -NoTypeInformation
+$RegistryIOC | Select PSComputerName,Key,IOC,Data,Generation,DependOnService,Description,DisplayName,ImagePath | Sort-Object -Property PSComputerName | Export-Csv -Path ($outputFolder + "RegIOC.csv") -NoTypeInformation
+Format-Events -logs $SecurityLog | Select IP,Time,EventID,Index,AuthPackage,@{n="ImportantMessage";e={Write-ImportantMessage -EventID $_.EventID -AuthPackage $_.AuthPackage}} | 
+Sort-Object -Property IP,Index | Export-Csv -Path ($outputFolder + "Events.csv") -NoTypeInformation
+$GroupedEvents | Export-CSV -Path ($outputFolder + "GroupedEvents.csv") -NoTypeInformation
+$BaselineHash | Export-CSV -Path ($outputFolder + "BaselineHash.csv") -NoTypeInformation
+$RecentModFiles | Export-CSV -Path ($outputFolder + "RecentModFiles.csv") -NoTypeInformation
+$Firewall | Select InstanceID,LocalAddress,LocalPort,RemoteAddress,RemotePort,Protocol,Action,PSComputerName | Export-CSV -Path ($outputFolder + "firewall.csv") -NoTypeInformation
+$Prefetch | Export-CSV -Path ($outputFolder + "prefetch.csv") -NoTypeInformation
+
+
+$BaselineInfo = [PSCustomObject] @{
+                                
+        Process            = $Process
+        Service            = $Service
+        OS                 = $OS
+        LocalGroups        = $LocalGroups
+        LocalGroupMembers  = $LocalGroupMembers
+        Users              = $Users
+        LoggedOnUsers      = $LoggedOnUsers
+        SchTasks           = $SchTasks
+        Connections        = $Connections
+        Shares             = $Shares
+        RegistryStartups   = $RegistryStartups
+        RegistrySchTasks   = $RegistrySchTasks
+        RegistryIOC        = $RegistryIOC
+        SecurityLog        = $SecurityLog
+        SecurityLogFormat  = $SecurityLogFormat
+        CriticalEvents     = $CriticalEvents
+        GroupedEvents      = $GroupedEvents
+        Baseline           = $Baseline
+        RecentModFiles     = $RecentModFiles
+        Firewall           = $Firewall 
+        Prefetch           = $Prefetch }
+
+$BaselineInfo | Export-Clixml -Path ($outputFolder + "BaselineInfo.xml")
 
